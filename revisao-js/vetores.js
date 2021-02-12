a86144b155caa18d6c46b052385b5b97000c932c
douglas-regatieri <datacode.comercial@gmail.com>
2021-02-12 01:25:42 +0000

    Revisão JS 11/02

---
+++ revisao-js/vetores.js
@@ -0,0 +1,9 @@
+let frutas = ['maça', 'Laranja', 'pera', 'uva', 'mamão', 'banana']
+
+let ultimaFruta = frutas.pop()
+
+console.log(frutas)
+
+let tirarPrimeira = frutas.shift()
+
+console.log(frutas)
